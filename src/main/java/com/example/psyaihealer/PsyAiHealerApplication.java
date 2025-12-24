6685f16c9b3e6e849143c70563f239e2d000ad56
lyl <3576303381@qq.com>
2025-12-24 16:16:01 +0800

    first commit

---
+++ src/main/java/com/example/psyaihealer/PsyAiHealerApplication.java
@@ -0,0 +1,13 @@
+package com.example.psyaihealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyAiHealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyAiHealerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/psyaihealer/PsyAiHealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psyaihealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyAiHealerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
